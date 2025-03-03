3220ed5daf10443d15d6c82d08569a69061ba304
ItSnOtNoOkIeBeAr <matthew867@gmail.com>
2025-03-03 21:43:32 +0800

    Configured Git identity and pushed changes

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'TiTE nI jAypEE',
       theme: ThemeData(
         // This is the theme of your application.
         //
@@ -106,7 +106,7 @@ class _MyHomePageState extends State<MyHomePage> {
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             const Text(
-              'You have pushed the button this many times:',
+              'Mahal ko si baby:',
             ),
             Text(
               '$_counter',
